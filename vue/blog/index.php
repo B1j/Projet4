6aceb666674a4ed54e8fe47933f6b0c445308c8c
B1j <beinje@gmail.com>
2017-08-25 15:35:41 +0200

    Point de départ du projet

---
+++ vue/blog/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Billet simple pour l'Alaska</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link href="vue/blog/style.css" rel="stylesheet" type="text/css" /> 
+    </head>
+        
+    <body>
+        <h1>Billet simple pour l'Alaska</h1>
+ 
+        <?php
+        foreach($billets as $billet)
+        {
+        ?>
+        <div class="news">
+            <h3>
+                <?php echo $billet['titre']; ?>
+                <em>le <?php echo $billet['date_creation_fr']; ?></em>
+            </h3>
+            
+            <p>
+                <?php echo $billet['contenu']; ?>
+                <br />
+                <em><a href="commentaires.php?billet=<?php echo $billet['id']; ?>">Commentaires</a></em>
+            </p>
+        </div>
+        <?php
+        }
+        ?>
+    </body>
+</html>
